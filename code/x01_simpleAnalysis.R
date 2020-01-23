1c15b1d09891c962d47856fc69b0d13bd68b72e5
Jon <jonsmitch2@gmail.com>
2020-01-23 12:00:03 -0500

    basic data and code

---
+++ code/x01_simpleAnalysis.R
@@ -0,0 +1,30 @@
+setwd("C:\\Users\\jsm0036\\Dropbox\\Research\\diets\\data")
+outMat <- read.csv("longform.csv", stringsAsFactors=F)
+
+########## NON BAYESIAN ##########
+library(nlme)
+sMat <- outMat[order(as.character(outMat$genus)),]
+library(lme4)
+Model <- lmer(ppreymass ~ predmass + (1 | clade/subclade/species) + ( 1 | obstype) + (1 | study), data = sMat, control=lmerControl(optimizer="nloptwrap"))
+
+########## SIMPLE   ##############
+library(brms)
+bform <- bf( ppreymass ~ predmass + (1 | clade) + (1 | study) + (1 | obstype) )
+
+Prior <- c(
+	prior(student_t(3, 0, 5), class="Intercept", coef=""),
+	prior(student_t(3, 0, 5), class=b),
+	prior(cauchy(0, 5), class=sd)
+	)
+
+fit1 <- brm(
+		bform, 
+		data = outMat, 
+		family = gaussian(), 
+		chains = 2, 
+		warmup = 1000, 
+		iter = 1100, 
+		control=list(max_treedepth = 11, adapt_delta=0.9), 
+		prior = Prior, 
+		cores=1
+		)
